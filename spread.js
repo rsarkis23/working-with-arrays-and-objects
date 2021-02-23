b0a52dfb564916b8800ef2cb82603a695ff18df6
rsarkis23 <learning.r2@gmail.com>
2021-02-22 21:25:49 -0500

    spread problems

--- spread.js
+++ spread.js
@@ -19,7 +19,7 @@ let product = {
 
 // code here
 
-
+let productCopy = {...product}
 
 
 ////////// PROBLEM 2 //////////
@@ -30,7 +30,7 @@ let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
 
-
+let colorsCopy = {...colors}
 
 
 ////////// PROBLEM 3 //////////
@@ -44,7 +44,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+let result = addNums(...numbers)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +54,6 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1, ...arr2]
+
+console.log(combinedArrays)
